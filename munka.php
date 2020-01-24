6f4dc5d0c8c39ea34de96ae7623f59c3a83a94a8
Comtur <attila.neuberger@gmail.com>
2020-01-24 03:08:18 +0100

    Osztás 1

--- munka.php
+++ munka.php
@@ -21,6 +21,14 @@ $x = 35;
 print "<br /><br />" . $x - $y;
 print "Konfliktus lesz.";
 
+// Osztás
+//-------
+
+$egyik = 25;
+$masik = 5;
+
+print ($egyik / $masik) . "<br /><br />";
+
 // Itt a vége.
 
 ?>
